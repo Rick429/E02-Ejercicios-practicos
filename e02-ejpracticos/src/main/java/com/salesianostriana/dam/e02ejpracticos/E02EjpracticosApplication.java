d24e51fbb930d001bfb8b61f2a46a22239e1296b
Rick429 <richaristico@gmail.com>
2021-10-17 09:54:14 +0200

    Creado proyecto spring

---
+++ e02-ejpracticos/src/main/java/com/salesianostriana/dam/e02ejpracticos/E02EjpracticosApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.e02ejpracticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E02EjpracticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E02EjpracticosApplication.class, args);
+	}
+
+}
---
+++ e02-ejpracticos/src/test/java/com/salesianostriana/dam/e02ejpracticos/E02EjpracticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.e02ejpracticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E02EjpracticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
